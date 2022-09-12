2a4a8fac8a9f6fac96068dabebd186ef4f091df2
Sourav Bhadra <sourav_bhadra@persistent.com>
2022-09-12 13:42:40 +0530

    Initial Commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"practicewebapplication/pkg/config"
+	"practicewebapplication/pkg/handlers"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+
+	/* pat rounter - https://github.com/bmizerany/pat
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	*/
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
